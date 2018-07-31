a226505a4f82497dc1326d6ed9e026e30ae711e1
kelly <korevec@gmail.com>
2018-07-31 14:53:59 -0700

    init

---
+++ spec/atom-crafts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomCraftsView from '../lib/atom-crafts-view';
+
+describe('AtomCraftsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
